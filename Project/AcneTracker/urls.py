69951700150bc2918dc3cc1781ad2c1734806434
soihateu <ericsgjames@gmail.com>
2021-02-28 22:23:42 -0500

    Update Procfile

--- Project/AcneTracker/urls.py
+++ Project/AcneTracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AcneCalendar/', include('AcneCalendar.urls')),
+    path('', include('AcneCalendar.urls')),
 ]
