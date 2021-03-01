d1d8266bbec5ebd4f8851e878798532559342687
soihateu <ericsgjames@gmail.com>
2021-02-28 21:20:52 -0500

    heroku

--- AcneTracker/AcneTracker/settings.py
+++ AcneTracker/AcneTracker/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(__file__)
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'staticfiles'),
+)
